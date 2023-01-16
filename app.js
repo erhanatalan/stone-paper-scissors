362cb15c5022414d23d2fb90ea69bbd66161d36f
erhanatalan <erhanatalan2013@gmail.com>
2023-01-16 20:42:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const yourChoise= document.getElementById("your-choice")
+const pcChoise = document.getElementById("pc-choice")
+
+const select = document.querySelector(".select")
+let userSelect;
+let pcRandom;
+
+
+select.addEventListener("click", (e)=>{
+    //console.log(e.target.className);
+    //console.log(e.target.getAttribute("alt"));
+    userSelect= e.target.getAttribute("alt")
+    yourChoise.innerHTML= `<img src="./assets/${userSelect}.png"></img>`
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const tasImage = document.querySelector(".tas")
+// const kagitImage = document.querySelector(".kagit")
+// const makasImage = document.querySelector(".makas")
+
+// tasImage.addEventListener("click", (e)=> {
+//     console.log(e);
+//     yourChoise.innerHTML= `<img src="./assets/tas.png"></img>`
+// })
+// kagitImage.addEventListener("click", (e)=> {
+//     console.log(e);
+//     yourChoise.innerHTML= `<img src="./assets/kagit.png"></img>`
+// })
+// makasImage.addEventListener("click", (e)=> {
+//     console.log(e);
+//     yourChoise.innerHTML= `<img src="./assets/makas.png"></img>`
+// })
+
+
+
+
+
+
